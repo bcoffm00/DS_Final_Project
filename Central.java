5eeb33243672d7e9f7cf2d54823a9585ece14c34
Taldan00 <162502917+Taldan00@users.noreply.github.com>
2024-05-02 07:59:18 -0500

    Update Central.java ON 5/2/2024 8:00AM

--- Central.java
+++ Central.java
@@ -16,6 +16,10 @@ public class Central {
 //Control access to Server List
 public static Semaphore SL = new Semaphore(1);
 
+public static int Id = 0;
+
+
+
 //List of fitting room Servers
 public static ArrayList<Connection> Servers = new ArrayList<Connection>();
 	
@@ -25,6 +29,14 @@ public static ArrayList<Connection> getServers(){
 
 public static Semaphore getSL() {
 	return SL;
+}
+
+public static int getId() {
+	return Id;
+}
+
+public static void setId(int x) {
+	Id = x;
 }
 
 	public static void main(String[] args) throws IOException {
@@ -38,14 +50,15 @@ public static Semaphore getSL() {
 		System.out.println("Server started");
 
 	//Listens For Connections
+		
 		while (true) {
 			try {
 
 				Socket connection = controller.accept();
 
-				Connection x = new Connection(connection,port);
+				Connection x = new Connection(connection,port,Id);
 				x.start();
-
+				
 			} catch (Exception e) {
 				controller.close();
 				e.printStackTrace();
@@ -62,7 +75,8 @@ class Connection extends Thread {
 	String ClientIP = null;
 	String ServerIP = null;
 	int port;
-
+	int id;
+	String name;
 
 	Socket s;
 	Socket connection;
@@ -80,9 +94,12 @@ class Connection extends Thread {
 	private BufferedReader ControllerIn = null;
 	private PrintWriter ControllerOut = null;
 	
-	public Connection(Socket connection, int port) {
+	public Connection(Socket connection, int port, int Id) {
 		this.connection = connection;
 		this.port = port;
+		this.id = Id;
+		
+		Central.setId(Central.getId() + 1);
 	}
 	
 	//Checks if the connection is still alive
@@ -145,6 +162,7 @@ class Connection extends Thread {
 			ServerIP = connection.getInetAddress().getHostAddress();
 			System.out.println("Connected to fitting room at: " + ServerIP);
 		
+			name = "Server " + id + " at "+ ServerIP;
 			out.println("CONNECTED");
 			out.flush();
 			
@@ -201,6 +219,8 @@ class Connection extends Thread {
 			String hostaddress = FittingRoom.ServerIP;
 			Socket s = new Socket(hostaddress, 32005);
 			
+			String ServerName = FittingRoom.name;
+			name = "Client " + id + " at "+ ClientIP;;
 			
 			//Get input/output for fitting room server
 			ControllerOut= new PrintWriter(s.getOutputStream());
@@ -211,44 +231,38 @@ class Connection extends Thread {
 			ControllerOut.flush();
 			int task = 1;
 			
-			System.out.println("216");
+			System.out.println("234");
 			while(line != null) {
 				if(!Central.getServers().contains(FittingRoom)) {
-				System.out.println("218");	
+				System.out.println("237");	
 				
 				
 				
 				}else {
 					switch(task) {
-					case 0:
-						//Sends ready message to Server
-						System.out.println("226");
-						ControllerOut.println("ready");
-						ControllerOut.flush();
-						task++;
-						break;	
 					case 1:
 						//Reads from Server
-						System.out.println(231);
+						System.out.println(name + " At 245");
 						line = ControllerIn.readLine();
-						System.out.println(line);
+						System.out.println("From: " + ServerName + ":" + line);
 						task++;
 						break;
 						
 					case 2:
 						//Sends the client message from Server
-						System.out.println("239");
+						System.out.println(name + " At 253");
 						
-					
+						System.out.println("From: " + ServerName + ":" + line);
+				
 						if(line.contains("Client has entered room")) {
-							System.out.println(line);
+							
 							out.println(line);
 							out.flush();
 							task  = 3;
 							break;
 						}else if(line.equalsIgnoreCase("Both fitting rooms and waiting room are full. Please try again later.")){
 							index++;
-							task = 6;
+							task = 5;
 							break;
 						}else {
 						out.println(line);
@@ -259,16 +273,16 @@ class Connection extends Thread {
 						
 					case 3:
 						//Reads from Client
-						System.out.println("260");
+						System.out.println(name + " At 276");
 						line = input.readLine();
-						System.out.println(line);
+						System.out.println("From: " + name + ":" + line);
 						task++;
 						break;
 						
 					case 4:
 						//Sends Message to Server From Client
 						
-						System.out.println("269");
+						System.out.println(name + " At 285");
 						ControllerOut.println(line);
 						ControllerOut.flush();
 						task = 1;
@@ -276,7 +290,9 @@ class Connection extends Thread {
 						
 					case 5:
 						//Fitting Room is full
-						System.out.println(262);
+						System.out.println(name + " At 293");
+						
+						s.close();
 						FittingRoom =  getServerConnection();
 						if(FittingRoom == null) {
 							out.println("No fitting room servers at this time.");
@@ -285,9 +301,22 @@ class Connection extends Thread {
 							while(!line.equalsIgnoreCase("RECIEVED")){
 								line = input.readLine();
 							}
-						
+						line = null;
 						break;
+					}else {
+						hostaddress = FittingRoom.ServerIP;
+						s = new Socket(hostaddress, 32005);
+						ServerName = FittingRoom.name;
+						
+						ControllerOut= new PrintWriter(s.getOutputStream());
+						ControllerIn = new BufferedReader(new InputStreamReader(s.getInputStream()));
+						
+						
+						ControllerOut.println("FITQUERY");
+						ControllerOut.flush();
+						task = 1;
 					}
+						
 				}
 				
 				}
