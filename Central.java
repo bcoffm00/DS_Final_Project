8ef7479a0f993d8f7337761beb63102a90dd3fc5
Taldan00 <162502917+Taldan00@users.noreply.github.com>
2024-05-04 15:07:40 -0500

    Update Central.java 5/4/24 3:07PM

--- Central.java
+++ Central.java
@@ -22,30 +22,40 @@ public class Central{
 	public static ArrayList<Connection> getServerList() {
 		return ServerList;
 	}
-
+	
+	public static boolean checkList(Connection a) {
+		while(access.tryAcquire() != true) {
+			
+		}
+		boolean contained = ServerList.contains(a);
+		access.release();
+		return contained;
+	}
 
 	public static void main(String args[]) {
 		int port = 32005;
 		int n = 0;
 		
-		//Start Server
-		while(true) {
 		try {
-			ServerSocket server = new ServerSocket(port);
-			System.out.println("Server started");
-			
+		//Start Server
+		ServerSocket server = new ServerSocket(port);
+		System.out.println("Server started");
+		while(true) {
+
 			Socket connection = server.accept();
 			
 			Connection x = new Connection(connection);
-		
+			
+			x.start();
 			
 			
-		} catch (IOException e) {
+			}
+		}catch (IOException e) {
 		
 			e.printStackTrace();
 		}
 
-		}
+		
 		
 		
 	}
@@ -59,10 +69,11 @@ public class Central{
 	 *
 	 */
 	// Connection Class
-	static class Connection implements Runnable{
+	static class Connection extends Thread{
 		String type;
 		String ip;
-		int id = 0;
+		int ServerID = 0;
+		int ClientID = 0;
 		String name;
 		Socket connection;
 		
@@ -79,7 +90,7 @@ public class Central{
 		public Connection(Socket connection) {
 			this.connection = connection;
 			ip = connection.getInetAddress().getHostAddress();
-			id++;
+			
 		}
 		
 		/**
@@ -89,8 +100,8 @@ public class Central{
 		 */
 		//If type is server this is called in the run method
 		public void Server() {
-			System.out.println("Connected to Server " + id + " At " + ip);
-			name = type+"-"+id+"-"+ip;
+			System.out.println("Connected to Server " + ServerID + " At " + ip);
+			name = type+"-"+ServerID+"-"+ip;
 			
 			while(Central.getAccess().tryAcquire() != true) {
 				
@@ -125,61 +136,24 @@ public class Central{
 		/**
 		 * Manages the messages between a client and server
 		 * Reroutes clients if Server goes down
+		 * @throws IOException 
 		 * 
 		 */
 		//If type is client this is called in the run method
-		public void Client() {
+		public void Client(){
 			BufferedReader ServerIn = null;
 			PrintWriter ServerOut = null;
-			name = type+"-"+id+"-"+ip;
-			
-			Connection Server = getServerConnection();
-			
-			if(Server == null) {
-				connectionOut.println("NOT CONNECTED");
-				try {
-					connection.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-			}else {
-				connectionOut.println("CONNECTED," + Server.ip);
-			
-				 try {
-					boolean clientTurn = true;
+			System.out.println("Connected to Client " + ClientID + " At " + ip);
+			name = type+"-"+ClientID+"-"+ip;
+					int task = 0;
+					boolean clientTurn = false;
 					boolean serverTurn = false;
-					
-					int task = 1;
 					String line = "";
-					ServerOut = new PrintWriter(Server.connection.getOutputStream());
-					ServerIn = new BufferedReader(new InputStreamReader(Server.connection.getInputStream()));
+					Connection Server = null;
+					FIRST:
 					while(task < 9) {
-						if(!Central.getServerList().contains(Server)) {
-							connectionOut.println("DISCONNECT");
-							Server = getServerConnection();
-							
-							if(Server == null) {
-								connectionOut.println("NOT CONNECTED");
-								try {
-									connection.close();
-									return;
-								} catch (IOException e) {
-									// TODO Auto-generated catch block
-									e.printStackTrace();
-								}
-							
-							}else {
-								connectionOut.println("CONNECTED," + Server.ip);
-								ServerOut = new PrintWriter(Server.connection.getOutputStream());
-								ServerIn = new BufferedReader(new InputStreamReader(Server.connection.getInputStream()));
-								task = 1;
-							}
-							
-							
-							
-						}else if(clientTurn) {
+					try {
+						if(clientTurn) {
 							line = connectionIn.readLine();	
 							
 						}else if(serverTurn) {
@@ -187,12 +161,42 @@ public class Central{
 						}
 						
 						switch(task){
-						
+							// Tries to connect to FittingRoom
+						case 0:
+							Server = getServerConnection();
+							
+							if(Server == null) {
+								System.out.println("SENDING NOTCONNECTED TO: " + name);
+								connectionOut.println("NOTCONNECTED");
+								connectionOut.flush();
+								
+								return;
+								
+							}else {
+								connectionOut.println("CONNECTED," + Server.ip);
+								connectionOut.flush();
+								
+									clientTurn = true;
+									serverTurn = false;
+									
+									task = 1;
+									line = "";
+									Socket s = new Socket(Server.ip,32005);
+									
+									ServerOut = new PrintWriter(s.getOutputStream());
+									ServerIn = new BufferedReader(new InputStreamReader(s.getInputStream()));
+									
+									System.out.println("Sending [fitquery] TO: " + Server.name);
+									ServerOut.println("fitquery");
+									ServerOut.flush();
+									break;
+							}
 						case 1:
 							// Message Sent to Server from Client
 							System.out.println(name + " Sending: " + line + " to " + Server.name + "\n");
 							
 									ServerOut.println(line);
+									ServerOut.flush();
 									serverTurn = true;
 									clientTurn = false;
 									task = 2;
@@ -202,34 +206,37 @@ public class Central{
 							System.out.println(Server.name + " Sending: " + line + " to " + name + "\n");
 							if(line.equalsIgnoreCase("ENTERED")) {
 								connectionOut.println(line + "," + Server.ip);
+								connectionOut.flush();
 								clientTurn = true;
 								serverTurn = false;
 								task = 1;
+								break;
 							}
 							if(line.equalsIgnoreCase("WAITING")) {
 								connectionOut.println(line + "," + Server.ip);
+								connectionOut.flush();
 								clientTurn = false;
 								serverTurn = true;
 								task = 2;
+								break;
+								}
+							if(line.equalsIgnoreCase("RECEIVED")) {
+								connectionOut.println(line + "," + Server.ip);
+								connectionOut.flush();
+								clientTurn = false;
+								serverTurn = false;
+								return;
 							}
+							
+							}
+						}catch(Exception e) {
+							System.out.println("An error Occured");
+								task = 0;
+								break FIRST;
+							} 
 						}
+				
 					}
-				} catch (SocketException e) {
-					try {
-						connection.close();
-					} catch (IOException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		
-			
-		}
 		
 		/**
 		 * Checks if the string x can be parsed as an Integer
@@ -260,7 +267,7 @@ public class Central{
 			boolean alive = false;
 			SocketAddress address = new  InetSocketAddress(hostname,port);
 			Socket socket = new Socket();
-			int timeout = 2000;
+			int timeout = 5000;
 			PrintWriter out;
 			BufferedReader input;
 			try {
@@ -295,7 +302,7 @@ public class Central{
 		 */
 		// Gets server by using round robin
 		public Connection getServerConnection() {
-			System.out.println(153);
+			
 				while(Central.getAccess().tryAcquire() != true) {
 
 				}
@@ -335,15 +342,18 @@ public class Central{
 				connectionOut = new PrintWriter(connection.getOutputStream());
 				String line = connectionIn.readLine();
 				if(line.equalsIgnoreCase("SERVER")) {
+					ServerID++;
 					type = "SERVER";
 					Server();
 				}if(line.equalsIgnoreCase("CLIENT")){
+					ClientID++;
 					type = "Client";
 					Client();
 				}else {
 					connection.close();
 				}
 				
+				System.out.println("Closing Connection at " + name);
 				connection.close();
 				
 			} catch (IOException e) {
