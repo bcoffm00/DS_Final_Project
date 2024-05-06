ad1920c9f575fcaba22606f2a62c16276b8b83d4
bcoffm00 <157536462+bcoffm00@users.noreply.github.com>
2024-05-06 20:11:29 +0000

    Add files via upload

--- Client.java
+++ Client.java
@@ -1,15 +1,48 @@
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+/********************************************************
+ * Name:   		Brody Coffman, Tony Aldana and Yash Patel
+ * Problem Set:	Final Group project
+ * Due Date :	5/2/24
+ *******************************************************/
+
+
+/**
+ * The main client class containing all needed code for the client threads
+ */
 public class Client {
+	/**
+	 * the filewriter for logging
+	 */
+	static FileWriter logWriter;
+	/**
+	 * Main method that rips threads for each client
+	 * @param args contains nothing
+	 */
 	public static void main (String[] args) {
+		
+		try {
+			File myObj = new File("logs.txt");
+		      if (myObj.createNewFile()) {
+		        System.out.println("File created: " + myObj.getName());
+		      } else {
+		        System.out.println("File already exists.");
+		      }
+			logWriter = new FileWriter(myObj, true);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		final String CENTRAL_IP = "192.168.0.0";
 		final int CENTRAL_PORT = 32005;
 		final int NUM_OF_CLIENTS = 4;
-
+		
 		for (int i = 1; i <= NUM_OF_CLIENTS; i++) {
 			//Creates N number of threads as specified by the third program argument
 			try {
@@ -33,9 +66,24 @@ public class Client {
 		}
 		//end of main
 	}
+	
+	/**
+	 * Writes to our log file
+	 * @param a the message to be written
+	 */
+	public static void logWrite (String a) {
+		try {
+			logWriter.write(a);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 
-
+	/**
+	 * The code for each customer that is created in the main method
+	 */
 	private static class customerThread extends Thread {
 		private int port;
 		private String ip;
@@ -50,22 +98,42 @@ public class Client {
 		private Socket centralServer = null;
 		private PrintWriter toCentral = null;
 		private BufferedReader fromCentral = null;
-
+		
+		/**
+		 * Returns the thread number
+		 * @return threadNum
+		 */
 		private int getThreadNum() {
 			return threadNum;
 		}
+		/**
+		 * Sets the thread number
+		 * @param threadNum
+		 */
 		private void setThreadNum(int threadNum) {
 			this.threadNum = threadNum;
 		}
-
+		
+		/**
+		 * Gets the tName for the customer thread
+		 * @return tName
+		 */
 		private String getTName() {
 			return tName;
 		}
+		/**
+		 * Sets tName using the threadNum
+		 */
 		private void setTName() {
 			this.tName = ("Client " + this.getThreadNum());
 		}
-
+		
+		/**
+		 * Initial connection to establish this is a client thread to the central server
+		 * @throws IOException
+		 */
 		public void initConnection () throws IOException {
+			logWrite("\t\t" + this.getTName() + ", Attempting connection to central server located at <" + this.ip + ">");
 			System.out.println("\t\t" + this.getTName() + ", Attempting connection to central server located at <" + this.ip + ">");
 
 			try {
@@ -73,7 +141,7 @@ public class Client {
 			} catch (Exception ex) {
 				ex.printStackTrace();
 			}
-
+			logWrite("\t\t" + this.getTName() + ", Connection to central server established");
 			System.out.println("\t\t" + this.getTName() + ", Connection to central server established");
 
 			//Setting up input and output flow
@@ -86,47 +154,63 @@ public class Client {
 			//ignores output from method and assumes that it was sent successfully
 		}
 
-
+		/**
+		 * Sends a message to the central server
+		 * @param message the message to be sent
+		 * @param a which task the message is sent from for debugging uses
+		 * @return whether the message sent properly or not
+		 */
 		public boolean sendMessage (String message, int a) {
 			a++;
 			//Messaging handler for modularization
 			try {
-				System.out.println("Attempting to send message, [" + message + "] to central server at task: " + a);
+				//System.out.println("Attempting to send message, [" + message + "] to central server at task: " + a);
 				//Attempts to send a message via the print writer
 
 				this.toCentral.println(message.toUpperCase());
 				this.toCentral.flush();
 
-				System.out.println("Successfully sent message, [" + message + "] to central server at task: " + a);
+				//System.out.println("Successfully sent message, [" + message + "] to central server at task: " + a);
 				//If message sends successfully then return true
 				return true;
 			} catch (Exception e) {
 				//If message fails to send then return false
-				System.out.println("Failed to send message, [" + message + "] to central server at task: " + a);
+				//System.out.println("Failed to send message, [" + message + "] to central server at task: " + a);
 				return false;
 			}
 		}
-
+		
+		/**
+		 * Reads a message from the central server
+		 * @param a which task the method reads the message in
+		 * @return the string that is read
+		 */
 		public String readMessage (int a) {
 			String message = "";
 			a++;
 			//Message receiver handler for modularization
 			try {
-				System.out.println("Attempting to read in message from central server at task: " + a);
+				//System.out.println("Attempting to read in message from central server at task: " + a);
 				//attempts to read in message via bufferedReader
 
 				message = this.fromCentral.readLine();
 
 				//If message is read successfully then return message
-				System.out.println("Successfully read in message, [" + message + "] from central server at task: " + a);
+				//System.out.println("Successfully read in message, [" + message + "] from central server at task: " + a);
 				return message;
 			} catch (Exception ex) {
-				System.out.println("Failed to read in message from central server at task: " + a);
+				//System.out.println("Failed to read in message from central server at task: " + a);
 				//If message fails to be read then return empty string
 				return message;
 			}
 		}
-
+		
+		/**
+		 * Basic constructor for the customer
+		 * @param IP
+		 * @param port
+		 * @param threadNum
+		 */
 		public customerThread (String IP, int port, int threadNum) {
 			this.ip = IP;
 			this.port = port;
@@ -134,11 +218,14 @@ public class Client {
 			this.setThreadNum(threadNum);
 			this.setTName();
 
-			System.out.println(this.getTName() + ", Created");
+			//System.out.println(this.getTName() + ", Created");
 		}
 
 		//@OVERRIDES THREAD.RUN()
 		//Contains body of code for the thread
+		/**
+		 * Body of code for linear execution of the customer thread, contains basic fault tolerance
+		 */
 		public void run () {
 
 			//TASK 1 HERE
@@ -160,6 +247,7 @@ public class Client {
 					case 0:
 						//Task 0, THIS IS AN ERROR TASK, ALL UNKNOWN SEQUENCING RELATED ERRORS SHOULD BE REDIRECTED HERE
 						//Prints error message followed by setting task to whichever task closes connections
+						logWrite("Error in sequencing at " + errTask);
 						System.out.println("Error in sequencing at " + errTask);
 						task = 11;
 						break;
@@ -169,6 +257,7 @@ public class Client {
 						//Task 1, THIS IS AN ERROR TASK, ALL INPUT RELATED ERRORS SHOULD BE REDIRECTED HERE
 						//THE ONLY POSSIBLE SOURCES OF INPUT RELATED ERRORS SHOULD BE TASK 2, OR, TASK 4
 						//Prints error message followed by setting the task to whichever task closes connections
+						logWrite("Error in processing input at " + errTask);
 						System.out.println("Error in processing input at " + errTask);
 						task = 11;
 						break;
@@ -196,12 +285,14 @@ public class Client {
 						///ANYTHING PAST THIS POINT IN TASK 3 ASSUMES MESSAGING AND READING WAS SUCCESSFULL IN PRIOR TASKS
 						if (message.split(",")[0].equalsIgnoreCase("CONNECTED")) {
 							//MESSAGE HANDLING, MESSAGE READ IN SUCCESSFULLY DURING TASK 2
+							logWrite("\t\t\t" + this.getTName() + ", Connected to fitting room server located at <" + message.split(",")[1] + ">");
 							System.out.println("\t\t\t" + this.getTName() + ", Connected to fitting room server located at <" + message.split(",")[1] + ">");
 							//message successful move onto task 4
 							task++;
 							break;
 						} else if (message.equalsIgnoreCase("NOTCONNECTED")) {
 							//MESSAGE HANDLING, MESSAGE READ IN SUCCESSFULLY DURING TASK 2
+							logWrite("\t\t\t" + this.getTName() + ", No space in fitting rooms or no fitting rooms available");
 							System.out.println("\t\t\t" + this.getTName() + ", No space in fitting rooms or no fitting rooms available");
 							//message successful move onto task 8
 
@@ -243,6 +334,7 @@ public class Client {
 							////////////////////////////////////////////////////////
 						} else if (message.equalsIgnoreCase("disconnect")) {
 							//ERROR HANDLING, FITTING ROOM CRASHED
+							logWrite("\t\t\t\t" + this.getTName() + ", Fitting Room Crashed, Redirecting");
 							System.out.println("\t\t\t\t" + this.getTName() + ", Fitting Room Crashed, Redirecting");
 							//Redirects back to task 2 which will read a message and processes connected or not connected
 							task = 2;
@@ -252,12 +344,14 @@ public class Client {
 						///ANYTHING PAST THIS POINT IN TASK 6 ASSUMES MESSAGING AND READING WAS SUCCESSFULL IN PRIOR TASKS
 						if (message.split(",")[0].equalsIgnoreCase("ENTERED")) {
 							//MESSAGE HANDLING, MESSAGE READ IN SUCCESSFULLY DURING TASK 5
+							logWrite("\t\t\t\t" + this.getTName() + ", Entered changing room inside of Fitting room server located at " + message.split(",")[1]);
 							System.out.println("\t\t\t\t" + this.getTName() + ", Entered changing room inside of Fitting room server located at " + message.split(",")[1]);
 							//message successful move onto task 7
 							task++;
 							break;
 						} else if (message.split(",")[0].equalsIgnoreCase("WAITING")) {
 							//MESSAGE HANDLING, MESSAGE READ IN SUCCESSFULLY DURING TASK 5
+							logWrite("\t\t\t\t" + this.getTName() + ", Entered waiting room inside of Fitting room server located at " + message.split(",")[1]);
 							System.out.println("\t\t\t\t" + this.getTName() + ", Entered waiting room inside of Fitting room server located at " + message.split(",")[1]);
 							//Revert back to task 5 and wait for Entered message
 							task--;
@@ -308,17 +402,20 @@ public class Client {
 							////////////////////////////////////////////////////////
 						} else if (message.contains("DISCONNECT")) {
 							//ERROR HANDLING, FITTING ROOM CRASHED
+							logWrite("\t\t\t\t" + this.getTName() + ", Fitting Room Crashed, Redirecting");
 							System.out.println("\t\t\t\t" + this.getTName() + ", Fitting Room Crashed, Redirecting");
 							//Redirects back to task 2 which will read a message and processes connected or not connected
 							task = 2;
 							break;
 						} else if (message.contains("RECEIVED")) {
 							//MESSAGE HANDLING, MESSAGE READ IN SUCCESSFULLY DURING TASK 5
+							logWrite("\t\t\t\t" + this.getTName() + ", Exited fitting room, closing connections");
 							System.out.println("\t\t\t\t" + this.getTName() + ", Exited fitting room, closing connections");
 							//message successful move onto task 11
 							task++;
 							break;
 						} else {
+							logWrite("Error in task 10, closing connections");
 							System.out.println("Error in task 10, closing connections");
 							task++;
 							break;
@@ -361,5 +458,4 @@ public class Client {
 
 
 
-}
 }
